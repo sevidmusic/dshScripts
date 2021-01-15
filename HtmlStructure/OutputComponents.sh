1b38f81d9c2fa998ee21c2388c529d5707dcf722
sevidmusic <sdmwebsdm@gmail.com>
2021-01-14 23:53:45 -0500

    Continued development of HtmlStructure App Package. Defined initial Components. Made instance of HtmlStructure App via draftMakeApp.sh. Everything worked as expected.

--- HtmlStructure/OutputComponents.sh
+++ HtmlStructure/OutputComponents.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 # OutputComponents.sh
 
-# This file is where OutputComponents, and DynamicOutputComponents are defined.
-#
-# Example OutputComponent
-# dsh -n OutputComponent "${app_name}" OCName OCContainer 0 "Output ..."
-#
-# Example DynamicOutputComponent
-# dsh -n DynamicOutputComponent "${app_name}" DOCName DOCContainer 0 "DOCFile.php"
-#
-# Once defined, make sure to asssign each OutputComponent and each DynamicOutputComponent
-# to one of the Responses or GlobalResponses defined in Responses.sh or else it will not
-# be visible in the App's user interface.
-#
-# Elaborating on the examples above:
-# dsh -n OutputComponent "${app_name}" OCName OCContainer 0 "Output ..."
-# dsh -a "${app_name}" ResponseName OCName OCContainer OutputComponent
-#
-# dsh -n DynamicOutputComponent "${app_name}" DOCName DOCContainer 0 "DOCFile.php"
-# dsh -a "${app_name}" ResponseName DOCName DOCContainer DynamicOutputComponent
-#
-# For more information about the dsh --assign-to-response flag use: dsh -h -a
-
 ########################## DO NOT MODIFY THE FOLLOWING LINE UNLESS YOU KNOW WHAT YOU ARE DOING! ##########################
 set -o posix; logErrorMsg() { printf "\n\e[43m\e[30m%s\n\e[0m" "${1}" >> /dev/stderr; }; logErrorMsgAndExit1() { logErrorMsg "${1}"; exit 1; }; determineDirectoryPath() { local CURRENT_FILE_PATH CURRENT_DIRECTORY_PATH; CURRENT_FILE_PATH="${BASH_SOURCE[0]}"; while [ -h "$CURRENT_FILE_PATH" ]; do CURRENT_DIRECTORY_PATH="$(cd -P "$(dirname "$CURRENT_FILE_PATH")" >/dev/null 2>&1 && pwd)"; CURRENT_FILE_PATH="$(readlink "$CURRENT_FILE_PATH")"; [[ $CURRENT_FILE_PATH != /* ]] && CURRENT_FILE_PATH="$CURRENT_DIRECTORY_PATH/$CURRENT_FILE_PATH"; done; printf "%s" "$(cd -P "$(dirname "$CURRENT_FILE_PATH")" >/dev/null 2>&1 && pwd)"; }; loadLibrary() { [[ ! -x "${1}" ]] && logErrorMsg "Error! Failed to load ${1}!" && logErrorMsgAndExit1 "The script either does not exist, or is not executable."; . ${1} ${2}; }; loadLibrary "$(determineDirectoryPath)/config.sh";
 
 ########################################## Please place all dsh calls after this line ####################################
 
+dsh -n OutputComponent "${app_name}" Doctype OpeningHtml 0 "<!DOCTYPE html>"
+dsh -a "${app_name}" DoctypeOpenHtmlOpenHeadTitle Doctype OpeningHtml OutputComponent
+
+dsh -n OutputComponent "${app_name}" OpeningHtmlTag OpeningHtml 0.01 '<html lang="en">'
+dsh -a "${app_name}" DoctypeOpenHtmlOpenHeadTitle OpeningHtmlTag OpeningHtml OutputComponent
+
+dsh -n OutputComponent "${app_name}" OpeningHeadTag OpeningHtml 0.02 "<head>"
+dsh -a "${app_name}" DoctypeOpenHtmlOpenHeadTitle OpeningHeadTag OpeningHtml OutputComponent
+
+dsh -n DynamicOutputComponent "${app_name}" Title title 0.03 "Title.php"
+dsh -a "${app_name}" DoctypeOpenHtmlOpenHeadTitle Title title DynamicOutputComponent
+
+dsh -n OutputComponent "${app_name}" MetaComment meta 0 '<!-- meta data -->'
+dsh -a "${app_name}" Meta MetaComment meta OutputComponent
+
+dsh -n OutputComponent "${app_name}" StylesComment css 0 '<!-- css -->'
+dsh -a "${app_name}" Styles StylesComment css OutputComponent
+
+dsh -n OutputComponent "${app_name}" ScriptsComment javascript 0 '<!-- javascript  -->'
+dsh -a "${app_name}" Scripts ScriptsComment javascript OutputComponent
 
--- HtmlStructure/Responses.sh
+++ HtmlStructure/Responses.sh
@@ -1,19 +1,53 @@
 #!/bin/bash
 # Responses.sh
 
-
-# This file is where Responses, and GlobalResponses are defined.
-#
-# Example Response
-# dsh -n Response "${app_name}" ResponseName 0
-#
-# Example GlobalResponse
-# dsh -n GlobalResponse "${app_name}" GlobalResponseName 0
-#
-# For more information about the dsh --assign-to-response flag use: dsh -h -a
-
 ########################## DO NOT MODIFY THE FOLLOWING LINE UNLESS YOU KNOW WHAT YOU ARE DOING! ##########################
 set -o posix; logErrorMsg() { printf "\n\e[43m\e[30m%s\n\e[0m" "${1}" >> /dev/stderr; }; logErrorMsgAndExit1() { logErrorMsg "${1}"; exit 1; }; determineDirectoryPath() { local CURRENT_FILE_PATH CURRENT_DIRECTORY_PATH; CURRENT_FILE_PATH="${BASH_SOURCE[0]}"; while [ -h "$CURRENT_FILE_PATH" ]; do CURRENT_DIRECTORY_PATH="$(cd -P "$(dirname "$CURRENT_FILE_PATH")" >/dev/null 2>&1 && pwd)"; CURRENT_FILE_PATH="$(readlink "$CURRENT_FILE_PATH")"; [[ $CURRENT_FILE_PATH != /* ]] && CURRENT_FILE_PATH="$CURRENT_DIRECTORY_PATH/$CURRENT_FILE_PATH"; done; printf "%s" "$(cd -P "$(dirname "$CURRENT_FILE_PATH")" >/dev/null 2>&1 && pwd)"; }; loadLibrary() { [[ ! -x "${1}" ]] && logErrorMsg "Error! Failed to load ${1}!" && logErrorMsgAndExit1 "The script either does not exist, or is not executable."; . ${1} ${2}; }; loadLibrary "$(determineDirectoryPath)/config.sh";
 
 ########################################## Please place all dsh calls after this line ####################################
+
+# dsh -n GlobalResponse "${app_name}" GlobalResponseName 0
+# dsh -n Response "${app_name}" ResponseName 0
+
+
+# Apps relying on this App for the Html structure of their output MUST not define
+# any Responses or GlobalResponses at position 0, it is reserved for the Doctype,
+# opening <html> tag, opening <head> tag, and <title>...</title> tags.
+dsh -n GlobalResponse "${app_name}" DoctypeOpenHtmlOpenHeadTitle 0
+
+# Apps relying on this App for the Html structure of their output can either assign
+# OutputComponents or DynamicOutputComponents that define <meta> tags to the Meta
+# GlobalResponse, or if OutputComponents or DynmaicOutputComponents are defined
+# for <meta> tags that are intended to be shown in response to a specific request
+# they can be assigned to appropriate Responses whose position is 1.
+dsh -n GlobalResponse "${app_name}" Meta 1
+
+# Apps relying on this App for the Html structure of their output can either
+# assign OutputComponents or DynamicOutputComponents that define <style> or
+# <link> tags to the Meta GlobalResponse, or if OutputComponents or
+# DynmaicOutputComponents are defined for <style> or <link> tags that are
+# intended to be shown in response to a specific request they can be assigned
+# to appropriate Responses whose position is 2.
+dsh -n GlobalResponse "${app_name}" Styles 2
+
+# Apps relying on this App for the Html structure of their output can either assign
+# OutputComponents or DynamicOutputComponents that define <script> tags to the Meta
+# GlobalResponse, or if OutputComponents or DynmaicOutputComponents are defined
+# for <script> tags that are intended to be shown in response to a specific request
+# they can be assigned to appropriate Responses whose position is 3.
+dsh -n GlobalResponse "${app_name}" Scripts 3
+
+# Apps relying on this App for the Html structure of their output MUST not define
+# any Responses or GlobalResponses at position 4, it is reserved for the closing,
+# <head> tag, and opening <body> tag.
+dsh -n GlobalResponse "${app_name}" CloseHeadBody 4
+
+# All output that is intened to exist within the <body>...</body> tags can be assigned
+# either to the Content GlobalResponse, or a Response whose position is 5.
+dsh -n GlobalResponse "${app_name}" Content 5
+
+# The position of the CloseBodyCloseHtml GlobalResponse is set high on purpose to
+# attempt to avoid output shwoing up after the closing <body> and closing html
+# <tags>.
+dsh -n GlobalResponse "${app_name}" CloseBodyCloseHtml 9999999999999999
 
